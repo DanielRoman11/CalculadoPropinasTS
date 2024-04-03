5a8c2ffc4d840b04798a2867d8a1e9a7d66b74a6
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-04-03 06:20:44 +0000

    html mockup values

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Propinas from './hooks/usePropinas'
 
 function App() {
-  const { data, consumo } = Propinas();
+  const { data, consumo, addConsumo } = Propinas();
 
   return (
     <>
@@ -18,11 +18,18 @@ function App() {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map(item => (
-                            <tr className='odd:bg-white text-md lg:text-md even:bg-slate-200 hover:bg-slate-400 *:hover:text-white' key={item.id}>
-                                <th scope='col' className='font-medium text-gray-900 px-6 py-4 text-left'>{item.name}</th>
-                                <th scope='col' className='font-medium text-gray-900 px-6 py-4 text-left'>${item.price}</th>
-                                <th scope='col' className='font-medium px-6 py-4 text-left'><a href='#' className='hover:underline underline-offset-4 font-semibold text-indigo-600 hover:text-indigo-500'>+ Añadir</a></th>
+                        {data.map(food => (
+                            <tr className='odd:bg-white text-md lg:text-md even:bg-slate-200 hover:bg-slate-400 *:hover:text-white transition-colors' key={food.id}>
+                                <th scope='col' className='font-medium text-gray-900 px-6 py-4 text-left'>{food.name}</th>
+                                <th scope='col' className='font-medium text-gray-900 px-6 py-4 text-left'>${food.price}</th>
+                                <th scope='col' className='font-medium px-6 py-4 text-left'>
+                                    <button 
+                                    type='button' 
+                                    className='hover:underline underline-offset-4 font-semibold text-indigo-600 hover:text-indigo-500' 
+                                    onClick={() => addConsumo(food)}>
+                                        Añadir
+                                    </button>
+                                </th>
                             </tr>
                         ))}
                     </tbody>
@@ -33,7 +40,57 @@ function App() {
                 <fieldset>
                     <legend className='mb-5 py-2 border-b border-gray-200 text-gray-400 text-xl w-full font-bold'>Contenido</legend>
                     {consumo.length > 0 
-                        ? <p>Hay contenido</p>
+                        ? 
+                        <>
+                            <ul className=''>
+                                {
+                                    consumo.map(foodItem => (
+                                        <li className='grid grid-cols-3 items-center pb-4' key={foodItem.id}>
+                                            <div className='flex flex-col'>
+                                                <h3 className='text-sm font-semibold text-gray-800'>{foodItem.name}</h3>
+                                                <p className='text-xs text-gray-400'>${foodItem.price}</p>
+                                            </div>
+                                            <div className='rounded-full place-self-center px-3 py-1 border flex items-center justify-between *:text-xs text-center w-fit space-x-6 font-semibold'>
+                                                <a href="#">-</a>
+                                                <p>{foodItem.quantity}</p>
+                                                <a href="#">+</a>
+                                            </div>
+                                            <div className='place-self-center hover:rotate-90 hover:text-red-600 cursor-pointer transition-all'>
+                                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className=" icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                                            </div>
+                                        </li>
+                                    ))
+                                }
+                                <li className='grid grid-cols-3 items-center pb-4'>
+                                    <div className='flex flex-col'>
+                                        <h3 className='text-sm font-semibold text-gray-800'>Jugo de Naranja</h3>
+                                        <p className='text-xs text-gray-400'>$15</p>
+                                    </div>
+                                    <div className='rounded-full place-self-center px-3 py-1 border flex items-center justify-between *:text-xs text-center w-fit space-x-6 font-semibold'>
+                                        <a href="#">-</a>
+                                        <p>1</p>
+                                        <a href="#">+</a>
+                                    </div>
+                                    <div className='place-self-center hover:rotate-90 hover:text-red-600 cursor-pointer transition-all'>
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className=" icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                                    </div>
+                                </li>
+                                <li className='grid grid-cols-3 items-center pb-4'>
+                                    <div className='flex flex-col'>
+                                        <h3 className='text-sm font-semibold text-gray-800'>Pizza a la leña</h3>
+                                        <p className='text-xs text-gray-400'>$30</p>
+                                    </div>
+                                    <div className='rounded-full place-self-center px-3 py-1 border flex items-center justify-between *:text-xs text-center w-fit space-x-6 font-semibold'>
+                                        <a href="#">-</a>
+                                        <p>2</p>
+                                        <a href="#">+</a>
+                                    </div>
+                                    <div className='place-self-center hover:rotate-90 hover:text-red-600 cursor-pointer transition-all'>
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className=" icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                                    </div>
+                                </li>
+                            </ul>
+                        </>
                         : <p className='text-center font-semibold text-gray-600'>No se han seleccionado productos 😴</p>
                     }
                 </fieldset>
@@ -45,8 +102,8 @@ function App() {
                                 <input type="radio" id="minima" name="propina" value="minima" className="hidden peer" defaultChecked/>
                                 <label htmlFor="minima" className="inline-flex items-center justify-center w-full p-2 text-gray-500 bg-white rounded-lg cursor-pointer peer-checked:border-indigo-600 peer-checked:text-indigo-600 hover:text-gray-600 hover:bg-gray-100 border">
                                     <div className="block">
-                                        <div className="w-full text-md text-center text-balance mb-2 pb-4 font-semibold">Mínima 🙂</div>
-                                        <div className="w-full text-xs text-center text-pretty">Se cobrará un 10% del valor total</div>
+                                        <div className="w-full text-sm text-center text-balance mb-2 pb-4 font-bold">Mínima 🙂</div>
+                                        <div className="w-full text-xs text-center text-pretty">Se cobrará un <strong>10%</strong> extra del valor total</div>
                                     </div>
                                 </label>
                             </li>
@@ -54,8 +111,8 @@ function App() {
                                 <input type="radio" id="mediana" name="propina" value="mediana" className="hidden peer"/>
                                 <label htmlFor="mediana" className="inline-flex items-center justify-center w-full p-2 text-gray-500 bg-white rounded-lg cursor-pointer peer-checked:border-indigo-600 peer-checked:text-indigo-600 hover:text-gray-600 hover:bg-gray-100 border">
                                     <div className="block">
-                                        <div className="w-full text-md text-center text-balance mb-2 pb-4 font-semibold">Mediana 😏</div>
-                                        <div className="w-full text-xs text-center text-pretty">Se cobrará un 15% del valor total</div>
+                                        <div className="w-full text-sm text-center text-balance mb-2 pb-4 font-bold">Mediana 😏</div>
+                                        <div className="w-full text-xs text-center text-pretty">Se cobrará un <strong>15%</strong> extra del valor total</div>
                                     </div>
                                 </label>
                             </li>
@@ -63,8 +120,8 @@ function App() {
                                 <input type="radio" id="grande" name="propina" value="grande" className="hidden peer"/>
                                 <label htmlFor="grande" className="inline-flex items-center justify-center w-full p-2 text-gray-500 bg-white rounded-lg cursor-pointer peer-checked:border-indigo-600 peer-checked:text-indigo-600 hover:text-gray-600 hover:bg-gray-100 border">
                                     <div className="block">
-                                        <div className="w-full text-md text-center text-balance mb-2 pb-4 font-semibold">Generosa 🤑</div>
-                                        <div className="w-full text-xs text-center text-pretty">Se cobrará un 20% del valor total</div>
+                                        <div className="w-full text-sm text-center text-balance mb-2 pb-4 font-bold">Generosa 🤑</div>
+                                        <div className="w-full text-xs text-center text-pretty">Se cobrará un <strong>20%</strong> extra del valor total</div>
                                     </div>
                                 </label>
                             </li>
@@ -72,7 +129,7 @@ function App() {
                                 <input type="radio" id="vacia" name="propina" value="vacia" className="hidden peer" />
                                 <label htmlFor="vacia" className="inline-flex items-center justify-center w-full p-2 text-gray-500 bg-white rounded-lg cursor-pointer peer-checked:border-pink-500 peer-checked:text-pink-500 hover:text-gray-600 hover:bg-gray-100 border">                        
                                     <div className="block">
-                                        <div className="w-full text-md text-center text-balance mb-2 pb-4 font-semibold">Vacía 😵</div>
+                                        <div className="w-full text-sm text-center text-balance mb-2 pb-4 font-bold">Vacía 😵</div>
                                         <div className="w-full text-xs text-center text-pretty">Se cobrará el valor total sin añadidos</div>
                                     </div>
                                 </label>
@@ -83,18 +140,18 @@ function App() {
                 <fieldset className='*:text-gray-800'>
                     <legend className='mb-5 py-2 border-b border-gray-200 !text-gray-400 text-xl w-full font-bold'>Totales y Propina</legend>
                     <div className='mx-auto w-full flex items-center justify-center mb-10'>
-                        <div className='text-center w-full px-10 py-1 shadow-md rounded-full'>
-                            <p className='text-xs text-gray-400'>Tu recibo de pago es de</p>
+                        <div className='text-center w-full px-10 py-1 shadow-md rounded-3xl'>
+                            <p className='text-xs text-gray-400'>El valor de la orden es</p>
                             <p className='text-3xl font-bold py-2 text-indigo-600'>$110</p>
                         </div>
                     </div>
-                    <div className='*:text-md'>
-                        <p>Subtotal: <span className='font-bold float-right'>$100</span></p>
-                        <p>Propina: <span className='font-bold float-right'>$10</span></p>
-                        <p>Propina: <span className='font-bold float-right'>$10</span></p>
+                    <div className='*:text-xs *:w-full'>
+                        <p>Subtotal <span className='font-bold float-right'>$100</span></p>
+                        <p>Propina <span className='font-bold float-right'>$10</span></p>
+                        <p className='border-t mt-4 font-semibold !text-lg'>Total <span className='font-bold text-indigo-600 float-right'>$110</span></p>
                     </div>
                 </fieldset>
-                <button type="button" className='text-center w-full py-2 rounded-md bg-indigo-600 text-white'>Guardar Orden</button>
+                <button type="button" className='text-center w-full py-2 rounded-md bg-indigo-600 hover:bg-indigo-800 text-white'>Guardar Orden</button>
             </article>
         </div>
     </>
--- src/hooks/usePropinas.ts
+++ src/hooks/usePropinas.ts
@@ -1,14 +1,35 @@
 import { useState } from "react";
 import { menuItems } from "../data/db";
+import { Food, FoodItem } from "../interfaces";
 
 export default function usePropinas() {
+  const initializeConsumo: FoodItem[] = []
 
   const [ data ] = useState(menuItems);
+  const [ consumo, setConsumo ] = useState(initializeConsumo);
 
-  const [ consumo, setConsumo ] = useState([]);
+  const MAX_TIMES = 15
+
+  function addConsumo(food: Food) {
+    const id = food.id;
+  
+    if(consumo.find((item: Food) => item.id === id) === undefined) {
+      const newFoodItem: FoodItem = {...food, quantity: 1}
+      setConsumo([...consumo, newFoodItem]);
+    } else {
+      const consumoCopy: FoodItem[]= [...consumo];
+      const thisQuantity: FoodItem | undefined = consumoCopy.find((item) => item.id === id);
+      
+      if(thisQuantity !== undefined && thisQuantity.quantity < MAX_TIMES) {
+        thisQuantity.quantity++;
+        setConsumo([...consumoCopy]);
+      }
+    }
+  }
 
   return {
     data,
-    consumo
+    consumo,
+    addConsumo
   }
 }
